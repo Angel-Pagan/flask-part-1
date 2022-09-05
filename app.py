400b9e69751b06559d1f68fe6588c494dd824686
Angel-Pagan <110137259+Angel-Pagan@users.noreply.github.com>
2022-09-05 13:15:00 -0400

    App.py file created

---
+++ app.py
@@ -0,0 +1,26 @@
+""" Import necessary packages """
+from flask import Flask
+
+""" Step 1: Inside the repo, create a basic flask app that has a 'hello world' as the main index (/), 
+and create at least two additional pages """
+
+    # Assign variable "app" to Flask
+    app = Flask(__name__)
+
+
+    #Assign flask route and website message
+    @app.route('/')
+    def main():
+        return 'Hello World'
+
+    @app.route('/sbu') 
+    def sbu():
+        return 'Welcome to the Stony Brook University Test Page'
+
+    @app.route('/hants')
+    def hants():
+        return 'Hello Dr.Williams'
+
+    #Assign DNS 
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0', port=80)
